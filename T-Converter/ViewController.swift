537c89364df90770b5900a4871d6fa45e533b622
Yrii Fetkullin <02011994jkl@mail.ru>
2022-09-21 21:02:40 +0300

    Сделал конвертер температуры Добавил элементы в storyboard Настроил слайдер

--- T-Converter/ViewController.swift
+++ T-Converter/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet{
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+            slider.tintColor = .black
+            slider.maximumTrackTintColor = .blue
+        }
     }
 
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let tempetureCelsius = Int(round(sender.value))
+        celsiusLabel.text = "\(tempetureCelsius)ºC"
+        let fahrenheitTemperature = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitabel.text = "\(fahrenheitTemperature)ºF"
+    }
 }
 
